7325c9050cfd48951779965981df3cd187b20c31
Neha Seth <68896581+sethns@users.noreply.github.com>
2024-03-05 11:12:50 +0530

    Update wa_reports_lib.py

--- modules/wa_reports_lib.py
+++ modules/wa_reports_lib.py
@@ -68,34 +68,7 @@ def get_docs(name):
     return docs
 
 def get_summary(name : str, return_intermediate_steps=False):
-    
-    # map_prompt_template = """
-    #                     Write a summary of this chunk of text that includes the main points and any important details.
-    #                     {text}
-    #                     """
-
-    # map_prompt = PromptTemplate(template=map_prompt_template, input_variables=["text"])
-
-    # combine_prompt_template = """
-    #                     Write a concise summary of the following text delimited by triple backquotes.
-    #                     Return your response in bullet points which covers the key points of the text.
-    #                     ```{text}```
-    #                     BULLET POINT SUMMARY:
-    #                     """
-
-    # combine_prompt = PromptTemplate(
-    #     template=combine_prompt_template, input_variables=["text"]
-    # )
-
-    # map_reduce_chain = load_summarize_chain(
-    #     llm,
-    #     chain_type="map_reduce",
-    #     map_prompt=map_prompt,
-    #     combine_prompt=combine_prompt,
-    #     return_intermediate_steps=True,
-    # )
-
-
+  
     map_prompt_template = "{text}\n\nWrite a few sentences summarizing the above:"
     map_prompt = PromptTemplate(template=map_prompt_template, input_variables=["text"])
     
