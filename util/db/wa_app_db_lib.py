fd677c1ddf1b98bd9282bfe3bd3a5907387f22c6
Neha Seth <68896581+sethns@users.noreply.github.com>
2024-03-05 11:42:01 +0530

    Update wa_app_db_lib.py

--- util/db/wa_app_db_lib.py
+++ util/db/wa_app_db_lib.py
@@ -6,45 +6,12 @@ from sqlalchemy import make_url
 #importing necessary packages --- NEW
 import pandas as pd
 from azure.identity import AzureCliCredential
-# import structcls
 import struct
 from itertools import chain, repeat
 import pyodbc
 import os
 from dotenv import load_dotenv
-# from notebookutils.mssparkutils.credentials import getSecret
-
-# KEYVAULT_ENDPOINT = "https://aifabric.vault.azure.net/"
-# OPENAI_ENDPOINT = "https://fabric-demo.openai.azure.com/"
-# OPENAI_SERVICES_KEY = getSecret(KEYVAULT_ENDPOINT, "fabricgptkey")
-# OPENAI_SERVICES_LOCATION = "eastus2"
-
-# print("ke read from keyvault is : ",KEYVAULT_ENDPOINT)
 load_dotenv()
-
-
-# db_name = "dev.db"
-# db_local_dev = "postgresql://postgres:postgressneha@localhost:5432/dev.db"
-
-# def get_db_connection_postgres():
-#     conn = psycopg2.connect(host="localhost", port="5432", dbname=db_name, user="postgres",password = "postgressneha" )
-#     conn.autocommit = True
-#     return conn
-
-# def get_vector_store(table : str):
-#     connection_string = "postgresql://postgres:postgressneha@127.0.0.1:5432"
-#     url = make_url(connection_string)
-#     vector_store = PGVectorStore.from_params(
-#         database=db_name,
-#         host=url.host,
-#         password=url.password,
-#         port=url.port,
-#         user=url.username,
-#         table_name=table,
-#         embed_dim=1536,  # openai embedding dimension
-#     )
-#     return vector_store
-
 
 def get_db_connection():
     credential = AzureCliCredential()
