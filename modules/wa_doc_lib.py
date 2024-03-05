075b5898579b612e50d6e29157cf53b35ee0f892
Neha Seth <68896581+sethns@users.noreply.github.com>
2024-03-05 11:04:51 +0530

    Update wa_doc_lib.py

--- modules/wa_doc_lib.py
+++ modules/wa_doc_lib.py
@@ -67,7 +67,6 @@ def upload_file(file, category, entity, doc_source):
     if file:
         doc_name=file.name
         print(f"File name: {doc_name}")
-        #with open(os.path.join(path,doc_name), 'wb') as filehandle:
         with open(path+doc_name, "wb") as filehandle: 
             filehandle.write(file.getbuffer())
             filehandle.close()
@@ -90,106 +89,11 @@ def upload_file(file, category, entity, doc_source):
         neg="{:.2f}".format(r["negative"][0])
         neu="{:.2f}".format(r["neutral"][0])
 
-        #db = cs50.SQL("sqlite:///file.db")  # For SQLite, file.db must exist
-        #db = cs50.SQL("mysql://username:password@host:port/database")  # For MySQL
-        # pos = 0.2
-        # neg = 0.3
-        # neu = 0.8
         c.execute("UPDATE [wealth_advisor_warehouse].[dbo].[documents] SET sentiment_positive = ?, sentiment_negative = ?, sentiment_neutral = ? WHERE doc_name = ?", pos, neg, neu, doc_name)       
-        # mydb.execute("UPDATE [wealth_advisor_warehouse].[dbo].[documents] SET sentiment_positive = ?, sentiment_negative = ?, sentiment_neutral = ? WHERE doc_name = ?", pos, neg, neu, doc_name)
         print("File processing completed")
     else:    
         print("No file to Upload")
 
-
-# def upload_file_fabric(base_path, category, entity):
-#     """
-#     Traverses a directory to find files, uploads them, processes them,
-#     and adds their content to a Faiss index.
-#     """
-#     print("UPLOAD FUNCTION LOADING")
-    
-#     # base_path = r"C:/Users/sethn/Downloads/wealth_assistant/upload/"
-#     # base_path = r"C:/Users/sneha/Desktop/wealth_assistant/upload"
-
-#     d_path = os.getcwd().replace("\\", "/") + "/destination/"
-#     # d_path = r"C:/Users/sneha/Desktop/wealth_assistant/destination"
-
-#     for root, _, files in os.walk(base_path):
-#         print("going to read files in the location")
-#         print("Files present in the location : ",files)
-#         for file_name in files:
-#             print("step2 in reading files")
-#             source_file_path = os.path.join(root, file_name)  # Correctly specify the file path
-#             d_file_path = os.path.join(d_path, file_name)
-#             #dt_file_path = os.path.join(dt_path, file_name)
-
-#             # Correctly open the source file to read its content
-#             with open(source_file_path, 'rb') as source_file:  # Use source_file_path, not base_path
-#                 file_content = source_file.read()
-
-#             # 'file' object simulation
-#             file = BytesIO(file_content)
-#             doc_name  = file_name  # Simulate the 'name' attribute of file-like objects
-
-#             print(f"Uploading file {doc_name} for category {category} and entity {entity}")
-
-#             if file:
-#                 # Since 'file' is a BytesIO object, use 'getvalue()' to write its content
-#                 with open(d_file_path, "wb") as filehandle:
-#                     filehandle.write(file.getvalue())  # Write the file content to the destination
-
-#                 if entity == "...":
-#                     entity = None
-
-#                 # Assuming download_loader and UnstructuredReader are properly defined and imported
-#                 UnstructuredReader = download_loader("UnstructuredReader")
-#                 loader = UnstructuredReader()
-#                 docs = loader.load_data(dt_file_path)
-
-#                 # Assuming add_to_faiss is properly defined and imported
-#                 add_to_faiss(file.name, docs)
-                    
-#                 print(f"File {doc_name} uploaded and processed successfully.")
-
-#                 dbconn = get_db_connection()
-#                 with dbconn.cursor() as c:
-#                     sql = """INSERT INTO [wealth_advisor_lakehouse].[dbo].[documents] (category, doc_name, entity, doc_source) VALUES (?, ?, ?, ?);"""
-#                     params = (category, doc_name, entity, "Upload")
-#                     c.execute(sql, params)
-#                     dbconn.commit() 
-
-#                 r = slib.generate_sentiment(d_path+doc_name)
-#                 #generate_embeddings(file.name)
-#                 print(r)
-#                 pos="{:.2f}".format(r["positive"][0])
-#                 neg="{:.2f}".format(r["negative"][0])
-#                 neu="{:.2f}".format(r["neutral"][0])
-
-#                 conn1 = get_db_connection()
-#                 sql1 = "UPDATE [wealth_advisor_lakehouse].[dbo].[documents] SET sentiment_positive = ?, sentiment_negative = ?, sentiment_neutral = ? WHERE doc_name = ?"
-#                 params = (pos, neg, neu, doc_name)
-#                 with conn1.cursor() as c1:
-#                     c1.execute(sql1, params)
-#                     conn1.commit()
-
-#                 #db = cs50.SQL("sqlite:///file.db")  # For SQLite, file.db must exist
-#                 #db = cs50.SQL("mysql://username:password@host:port/database")  # For MySQL
-#                 #mydb.execute("UPDATE DOCUMENTS SET sentiment_positive = ?, sentiment_negative = ?, sentiment_neutral = ? WHERE doc_name = ?", pos, neg, neu, doc_name)
-#                 print("File processing completed")
-#             else:    
-#                 print("No file to Upload")
-                
-
-
-
-# def get_faiss_index():
-#     vector_store = FaissVectorStore.from_persist_dir("./storage/doc")
-#     storage_context = StorageContext.from_defaults(
-#         vector_store=vector_store, persist_dir="./storage/doc"
-#     )
-#     index = load_index_from_storage(storage_context=storage_context)
-#     return index
 
 def get_answer_from_docs_faiss(doc_query):
     print(f"QUERY *********{doc_query}")
@@ -218,51 +122,3 @@ def get_answer_from_docs_faiss(doc_query):
     print(pdf_names)
     
     return str(response),  pdf_names
-
-# def add_to_faiss(doc_name: str, msg_documents : List[Document]):
-#     # dimensions of text-ada-embedding-002
-#     d = 1536
-#     faiss_index = faiss.IndexFlatL2(d)
-
-#     try:
-#         vector_store = FaissVectorStore.from_persist_dir("./storage/doc")
-#         storage_context = StorageContext.from_defaults(
-#             vector_store=vector_store, persist_dir="./storage/doc"
-#         )
-#         index = load_index_from_storage(storage_context=storage_context)
-#         index_loaded = True
-#     except:
-#         index_loaded = False
-
-#     if index_loaded:
-#         for doc in msg_documents:
-#             doc.extra_info["filename"] = doc_name
-#             doc.metadata = {"filename": doc_name}
-#             index.insert(doc) # for doc in msg_documents )
-#         index.storage_context.persist(persist_dir="./storage/doc")
-#     else:
-#         print("###################Index does not exist - to be created now")
-#         vector_store = FaissVectorStore(faiss_index=faiss_index)
-#         storage_context = StorageContext.from_defaults(vector_store=vector_store)
-#         index = VectorStoreIndex.from_documents(
-#             msg_documents, storage_context=storage_context
-#         )
-#         index.storage_context.persist(persist_dir="./storage/doc")
-
-# CREATE TABLE DOCUMENTS (
-#   doc_id SERIAL PRIMARY KEY,
-#   category varchar(100) DEFAULT NULL,
-#   entity varchar(50) DEFAULT NULL,
-#   doc_name varchar(150) DEFAULT NULL,
-#   doc_source varchar(50) DEFAULT NULL,
-#   sentiment_positive float DEFAULT NULL,
-#   sentiment_negative float DEFAULT NULL,
-#   sentiment_neutral float DEFAULT NULL
-# );    
-    
-# with conn.session as s:
-#     s.execute(
-#         'INSERT INTO DOCUMENTS (doc_id, category, doc_name, entity, doc_source) VALUES (:doc_id, :category, :doc_name, :entity, :doc_source);',
-#         params=dict(doc_id=doc_id, category=category, doc_name=file.name, entity=entity, doc_source="Upload")
-#     )
-#     s.commit()
